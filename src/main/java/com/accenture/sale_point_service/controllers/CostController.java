11b52325bd25f7a4e04946a532d2d1fe80d9355b
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-04-11 16:49:05 -0300

    crud of cost controller

--- src/main/java/com/accenture/sale_point_service/controllers/CostController.java
+++ src/main/java/com/accenture/sale_point_service/controllers/CostController.java
@@ -1,7 +1,39 @@
 package com.accenture.sale_point_service.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.accenture.sale_point_service.dtos.CostDto;
+import com.accenture.sale_point_service.services.CostService;
+import com.accenture.sale_point_service.utils.ApiResponse;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/costs")
 public class CostController {
+
+    private final CostService costService;
+
+    public CostController(CostService costService) {
+        this.costService = costService;
+    }
+
+    @GetMapping("/all")
+    public ApiResponse<List<CostDto>> getAllCosts() {
+        return costService.getAllCosts();
+    }
+
+    @PostMapping("/create")
+    public ApiResponse<CostDto> createCost(@RequestBody CostDto costDto) {
+        return costService.createCost(costDto);
+    }
+
+    @DeleteMapping("/{fromId}-{toId}")
+    public ApiResponse<String> deleteCost(@PathVariable Long fromId, @PathVariable Long toId) {
+        return costService.deleteCost(fromId, toId);
+    }
+
+    @GetMapping("/direct-connections/{fromId}")
+    public ApiResponse<List<CostDto>> allDirectConnectionsFrom(@PathVariable Long fromId) {
+        return costService.getDirectConnectionsFrom(fromId);
+    }
 }
