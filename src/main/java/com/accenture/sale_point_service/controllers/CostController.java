9fd723a641fa0a7b13d27dd84f57758913ca5988
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-04-14 13:15:54 -0300

    add endpoint /shortest-path

--- src/main/java/com/accenture/sale_point_service/controllers/CostController.java
+++ src/main/java/com/accenture/sale_point_service/controllers/CostController.java
@@ -1,8 +1,12 @@
 package com.accenture.sale_point_service.controllers;
 
 import com.accenture.sale_point_service.dtos.CostDto;
+import com.accenture.sale_point_service.graph.ShortestPathResult;
 import com.accenture.sale_point_service.services.CostService;
+import com.accenture.sale_point_service.graph.GraphService;
 import com.accenture.sale_point_service.utils.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,9 +16,11 @@ import java.util.List;
 public class CostController {
 
     private final CostService costService;
+    private final GraphService graphService;
 
-    public CostController(CostService costService) {
+    public CostController(CostService costService, GraphService graphService) {
         this.costService = costService;
+        this.graphService = graphService;
     }
 
     @GetMapping("/all")
@@ -35,5 +41,15 @@ public class CostController {
     @GetMapping("/direct-connections/{fromId}")
     public ApiResponse<List<CostDto>> allDirectConnectionsFrom(@PathVariable Long fromId) {
         return costService.getDirectConnectionsFrom(fromId);
+    }
+
+
+    @GetMapping("/shortest-path")
+    public ResponseEntity<ShortestPathResult> getShortestPath(@RequestParam Long from, @RequestParam Long to) {
+        ShortestPathResult result = graphService.findShortestPath(from, to);
+        if (result.getTotalCost() == -1) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(result);
+        }
+        return ResponseEntity.ok(result);
     }
 }
