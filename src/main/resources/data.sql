2d2ac88dd40d95fb99dbfce4ada2552cbe7f2d34
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-06 08:56:55 -0300

    update data.sql without using hardcoded ids

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,16 @@
 -- SalePointEntity
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (1, 'CABA');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (2, 'GBA_1');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (3, 'GBA_2');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (4, 'Santa Fe');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (5, 'Córdoba');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (6, 'Misiones');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (7, 'Salta');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (8, 'Chubut');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (9, 'Santa Cruz');
-INSERT INTO sale_point_entity (sale_point_id, name) VALUES (10, 'Catamarca');
+INSERT INTO sale_point_entity (name) VALUES ('CABA');
+INSERT INTO sale_point_entity (name) VALUES ('GBA_1');
+INSERT INTO sale_point_entity (name) VALUES ('GBA_2');
+INSERT INTO sale_point_entity (name) VALUES ('Santa Fe');
+INSERT INTO sale_point_entity (name) VALUES ('Córdoba');
+INSERT INTO sale_point_entity (name) VALUES ('Misiones');
+INSERT INTO sale_point_entity (name) VALUES ('Salta');
+INSERT INTO sale_point_entity (name) VALUES ('Chubut');
+INSERT INTO sale_point_entity (name) VALUES ('Santa Cruz');
+INSERT INTO sale_point_entity (name) VALUES ('Catamarca');
 
--- CostEntity (usa composite key: from_id, to_id)
+-- CostEntity (uses composite key: from_id, to_id)
 INSERT INTO cost_entity (from_id, to_id, cost) VALUES (1, 2, 2);
 INSERT INTO cost_entity (from_id, to_id, cost) VALUES (1, 3, 3);
 INSERT INTO cost_entity (from_id, to_id, cost) VALUES (2, 3, 5);
