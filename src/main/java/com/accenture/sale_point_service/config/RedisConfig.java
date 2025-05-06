175605f48c5c4373091614be7a9caf57e8892c07
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-05-06 12:43:54 -0300

    create RedisConfig

---
+++ src/main/java/com/accenture/sale_point_service/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.accenture.sale_point_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .prefixCacheNameWith("sale-point::")
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()
+                        )
+                );
+    }
+}
