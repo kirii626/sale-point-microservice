4879dde474c01abcc4f9c04ad68e1bdc0d5428ee
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-02 18:47:25 -0300

    fix: amend and improve RedisConfig

--- src/main/java/com/accenture/sale_point_service/config/RedisConfig.java
+++ src/main/java/com/accenture/sale_point_service/config/RedisConfig.java
@@ -3,38 +3,28 @@ package com.accenture.sale_point_service.config;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @Configuration
-@EnableCaching
 public class RedisConfig {
 
     @Bean
-    public GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.activateDefaultTyping(
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        ObjectMapper redisObjectMapper = new ObjectMapper();
+        redisObjectMapper.activateDefaultTyping(
                 LaissezFaireSubTypeValidator.instance,
                 ObjectMapper.DefaultTyping.NON_FINAL,
                 JsonTypeInfo.As.PROPERTY
         );
-        return new GenericJackson2JsonRedisSerializer(objectMapper);
-    }
 
+        GenericJackson2JsonRedisSerializer serializer =
+                new GenericJackson2JsonRedisSerializer(redisObjectMapper);
 
-    @Bean
-    public RedisCacheConfiguration redisCacheConfiguration(GenericJackson2JsonRedisSerializer serializer) {
-        System.out.println("Configuring RedisCacheConfiguration with GenericJackson2JsonRedisSerializer...");
         return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(serializer)
-                );
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
     }
-
 }
