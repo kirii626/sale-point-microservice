daea3a3f1ea1cbecd8be723fd1b306778d9ee5e6
Alejandra Justiniano <kiaraalejandrajustinianoolmos@gmail.com>
2025-06-10 14:50:32 -0300

    fix(graph): add no args constructor

--- src/main/java/com/accenture/sale_point_service/graph/PathPoint.java
+++ src/main/java/com/accenture/sale_point_service/graph/PathPoint.java
@@ -2,9 +2,11 @@ package com.accenture.sale_point_service.graph;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PathPoint {
 
     private Long id;
--- src/main/java/com/accenture/sale_point_service/graph/ShortestPathResult.java
+++ src/main/java/com/accenture/sale_point_service/graph/ShortestPathResult.java
@@ -2,11 +2,13 @@ package com.accenture.sale_point_service.graph;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ShortestPathResult {
 
     private Long totalCost;
